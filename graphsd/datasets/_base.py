120a2073618da64431a09bca3974c735dd7a4572
rebelosa <claudio84@gmail.com>
2022-03-12 17:32:58 +0000

    clean code

--- graphsd/datasets/_base.py
+++ graphsd/datasets/_base.py
@@ -4,16 +4,17 @@ from os.path import dirname
 module_path = dirname(__file__)
 
 
-def load_playgroundA():
+def load_playground_a():
 
-    position_data = pd.read_csv(module_path + "./data/playgrounda_position_data_anonymized.csv")
-    social_data = pd.read_csv(module_path + "./data/playgrounda_social_data_anonymized.csv")
+    position_data = pd.read_csv(module_path + "./data/playground_a_position_data_anonymized.csv")
+    social_data = pd.read_csv(module_path + "./data/playground_a_social_data_anonymized.csv")
 
     return position_data, social_data
 
-def load_playgroundB():
 
-    position_data = pd.read_csv(module_path + "./data/playgroundb_position_data_anonymized.csv")
-    social_data = pd.read_csv(module_path + "./data/playgroundb_social_data_anonymized.csv")
+def load_playground_b():
+
+    position_data = pd.read_csv(module_path + "./data/playground_b_position_data_anonymized.csv")
+    social_data = pd.read_csv(module_path + "./data/playground_b_social_data_anonymized.csv")
 
     return position_data, social_data
